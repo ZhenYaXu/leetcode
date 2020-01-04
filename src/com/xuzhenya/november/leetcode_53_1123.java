1b6d308d82c208e9a4a79458e7579b5181b12b9b
ZhenYaXu <xuzhenya1997@gmail.com>
2020-01-04 20:57:55 +0800

    upodate

---
+++ src/com/xuzhenya/november/leetcode_53_1123.java
@@ -0,0 +1,32 @@
+package com.xuzhenya.november;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: https://leetcode-cn.com/problems/maximum-subarray/
+ *
+ * @author: xuzhenya
+ * @date: 2019.11.03
+ */
+public class leetcode_53_1123 {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        //sum[i] nums数组前i个最大字串和
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(sum[i-1] > 0)
+                sum[i] = nums[i] + sum[i-1];
+            else
+                sum[i] = nums[i];
+        }
+        Arrays.sort(sum);
+        return sum[sum.length-1];
+    }
+
+}
