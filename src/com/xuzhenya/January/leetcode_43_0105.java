3c989214f70e4610e56ed6d63e5fb57abc8884da
ZhenYaXu <xuzhenya1997@gmail.com>
2020-01-05 16:33:27 +0800

    add 0104

---
+++ src/com/xuzhenya/January/leetcode_43_0105.java
@@ -0,0 +1,14 @@
+package com.xuzhenya.January;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: https://leetcode-cn.com/problems/multiply-strings/
+ *
+ * @author: xuzhenya
+ * @date: 2020.01.04
+ */
+public class leetcode_43_0105 {
+    public String multiply(String num1, String num2) {
+
+    }
+}
---
+++ src/com/xuzhenya/January/leetcode_50_0105.java
@@ -0,0 +1,31 @@
+package com.xuzhenya.January;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: https://leetcode-cn.com/problems/powx-n/
+ *
+ * @author: xuzhenya
+ * @date: 2020.01.04
+ */
+public class leetcode_50_0105 {
+    // 快速幂 （分奇偶讨论）
+    private double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+}
